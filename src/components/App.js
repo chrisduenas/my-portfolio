be4222d5a697feb97de493c90fc383037a6433e7
Christopher Duenas <chrisduenasonline@gmail.com>
2018-08-13 20:48:38 -0400

    Header layout

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my portfolio page</h1>
+            <hr/>
+            <p className="App-title-bottom">Follow me on:</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
