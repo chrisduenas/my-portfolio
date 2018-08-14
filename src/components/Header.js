f65398aa4fe3c2a52d000b17297c3d1be01dc716
Christopher Duenas <chrisduenasonline@gmail.com>
2018-08-14 18:58:27 -0400

    adds grid to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,23 @@ export default class Header extends Component {
         return (
             <div>
                 <h1 className="name">Chris Duenas</h1>
+                <div className="roles ui grid">
+                    <div className="four wide column">
+                        <p>Web Developer</p>
+                    </div>
+                    <div className="four wide column">
+                        <p>Mixologist</p>
+                    </div>
+                    <div className="four wide column">
+                        <p>Actor</p>
+                    </div>
+                    <div className="four wide column">
+                        <p>Fitness Enthusiast</p>
+                    </div>
+                </div>
+                <div class="ui divider"></div>
             </div>
+
         )
     }
 }
