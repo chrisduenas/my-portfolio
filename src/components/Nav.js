dece1afb8e48692338faea09ccabe8e36c0ff8fa
Christopher Duenas <chrisduenasonline@gmail.com>
2018-08-15 17:39:53 -0400

    adds navigation onclick events

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
+import { Dropdown, Menu } from 'semantic-ui-react';
 
 
 
 export default class Nav extends Component {
+    state ={}
+
+    handleItemClick = (e, { name } ) => this.setState({ activeItem: name})
+
     render() {
+        const { activeItem } = this.state
         return (
-                <div className="ui inverted segment">
-                    <div className="ui inverted pointing fixed menu">
-                        <div className="item left">
-                            <a href="#">Chris Duenas</a>
-                        </div>
-                        <div className="item">
-                            <a href="#">About Me</a>
-                        </div>
-                        <div className="item">
-                            <a href="#">Projects</a>
-                        </div>
-                    </div>
-                 </div> 
+            <Menu right horizontal borderless inverted>
+                <Dropdown item text="Chris Duenas" className="chrisduenas">
+                    <Dropdown.Menu>
+                        <Dropdown.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick}>Home</Dropdown.Item>
+                        <Dropdown.Item name="about" active={activeItem === 'about'} onClick={this.handleItemClick}>About</Dropdown.Item>
+                        <Dropdown.Item name="projects" active={activeItem === 'projects'} onClick={this.handleItemClick}>Projects</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu>
+
         )
     }
 };
+
