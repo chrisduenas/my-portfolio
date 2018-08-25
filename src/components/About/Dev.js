4bdcd929ef54bacee840fae244a0af6170da13d0
Christopher Duenas <chrisduenasonline@gmail.com>
2018-08-24 20:04:18 -0400

    refactoring toggle functions

--- src/components/About/Dev.js
+++ src/components/About/Dev.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
+// import roles from '.../data/roles.json';
+// import { Segment } from 'semantic-ui-react';
 
 export default class Dev extends Component {
+    constructor(props) {
+        super(props)
+        this.titleWasClicked = this.titleWasClicked.bind(this)
+        this.toggleRoles = this.toggleRoles.bind(this)
+        this.state = {
+            showRoles: true
+        }
+    }
+
+    titleWasClicked(event) {
+        event.preventDefault()
+        const { dataCallback } = this.props
+        dataCallback('hello world!');
+        console.log(dataCallback);
+    }
+
+    toggleRoles(event){
+        event.preventDefault()
+        this.setState({
+            showRoles: !this.state.showRoles
+        })
+    }
+
     render() {
+        const { item } = this.props
+        const { showRoles } = this.state
         return (
-            <Grid>
-                <Segment>
-                    <div>
-                        <h5>Full Stack Developer</h5>
-                        <p>I am a full stack Dev</p>
-                    </div>
-                </Segment>
-            </Grid>
+            <div>
+                <h5 onClick={this.titleWasClicked}>{item.title}</h5>
+                { showRoles === true ? <p>{item.description}</p> : ''}
+            </div>
         )
     }
 }
