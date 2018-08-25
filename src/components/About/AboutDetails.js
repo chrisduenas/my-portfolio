35b3cddce3c3557399f07f9d092365f48c2c3385
Christopher Duenas <chrisduenasonline@gmail.com>
2018-08-25 12:42:51 -0400

    refactoring role description

--- src/components/About/AboutDetails.js
+++ src/components/About/AboutDetails.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react'
 
 export default class AboutDetails extends Component {
-    constructor(props) {
+    constructor(props){
         super(props)
-        this.titleWasClicked = this.titleWasClicked.bind(this)
-        this.toggleContent = this.toggleContent.bind(this)
-        this.state = {
-            showContent: false
-
-        }
-    }
-    titleWasClicked(event) {
-        event.preventDefault()
-        const { dataCallback } = this.props
-        dataCallback(event)
-    }
-
-    toggleContent(event) {
-        event.preventDefault()
-        const { toggle } = this.props
-        toggle(event)
-        // this.setState({ 
-        //     showContent: !this.state.showContent
-        // })
-    }
-    render(){
-        const { person } = this.props
+}
+    render(){  
         return (
-            <div>
-                 <div className="ui divider"></div>
-                    <div className="ui container">
-                        <p onClick={this.toggleContent}>{person}</p> 
-                    </div>
-            </div>
-        )
+                <div>
+                    <p>person</p>
+                </div>
+                )
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
 import '../CSS/Header.css';
 import About from './About';
-import roles from '../data/roles.json';
 
 export default class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            roles: roles,
-            segments : false,
-         }
-         this.toggleRole = this.toggleRole.bind(this);
-    }
-
-    toggleRole(event) {
-        event.preventDefault()
-        this.setState({ 
-        segments : !this.state.segments,
-        })
-    }
-    
     render() {
         return (
             <div className="header">
                 <div className="ui container">
                     <h1 className="name">Chris Duenas</h1>
                 </div>
-                <About/>
+                <div className="ui container">
+                    <About/>
+                </div>
             </div>
 
         )
