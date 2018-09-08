e52b221bc2c9e8e4c5ca1b44742d21c286c40e4f
Christopher Duenas <chrisduenasonline@gmail.com>
2018-09-08 14:52:19 -0400

    adds margins to segment

--- src/components/About/AboutDetails.js
+++ src/components/About/AboutDetails.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
 
 export default class AboutTitle extends Component {
     render() {
         const {description} = this.props.person;
-        console.log({description})
         return(
-            <div className="roles">
-                 <p>{description}</p>
+            <div className="about-description">
+                <Segment stacked>
+                        <p>{description}</p>
+                </Segment>
             </div>
         )
     }
