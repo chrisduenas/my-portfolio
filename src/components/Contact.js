81e32c0c543b4b69f32e411c841801c0514ba403
Christopher Duenas <chrisduenasonline@gmail.com>
2018-09-24 22:58:55 -0400

    Resized Projects and contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Icon } from 'semantic-ui-react';
+import { Grid, Icon, Divider } from 'semantic-ui-react';
 import '../CSS/Contact.css';
 
 export default class Contact extends Component {
@@ -7,8 +7,18 @@ export default class Contact extends Component {
         return (
             <div className="contact">
                 <div className="ui container">
-                    <h2 className="contact-title">Contact</h2>
+                    <h2 className="contact-title">Contact Me</h2>
+                <div className="resume">
+                        <a className="social-link" href='https://www.dropbox.com/home?preview=chrisduenas+resume+2018.pdf'>
+                            <p><strong>Resume</strong></p>
+                            <Icon name='file alternate outline' size='big' /> 
+                        </a>
+                        <a className="social-link" href='mailto:chrisduenasonline@gmail.com'>
+                            <p><strong>Email</strong></p>
+                            <Icon name='mail' size='big' /> 
+                        </a>
                 </div>
+                <Divider section/>
                     <Grid className="columns">
                         <Grid.Row>
                             <Grid.Column>
@@ -31,16 +41,12 @@ export default class Contact extends Component {
                                 <Icon name='twitter' size='big' /> 
                                 </a>
                             </Grid.Column>
-                            <Grid.Column>
-                            <a className="social-link" href='https://www.dropbox.com/home?preview=chrisduenas+resume+2018.pdf'>
-                                <Icon name='file alternate outline' size='big' /> 
-                                </a>
-                            </Grid.Column>
                         </Grid.Row>
                     </Grid>
                     <div className="ui container">
                         <p>Copyright Chris Duenas 2018</p>
                     </div>
+            </div>
             </div>
         )
     }
