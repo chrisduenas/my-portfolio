85e6398cd49b5a3eccb3422a47fe9b881d5c14d6
Christopher Duenas <chrisduenasonline@gmail.com>
2018-09-29 13:03:59 -0400

    Adds Grid to About me section

--- src/components/AboutMe/AboutSection.js
+++ src/components/AboutMe/AboutSection.js
@@ -1,43 +1,73 @@
 import React, { Component } from 'react';
-import { Image, Responsive } from 'semantic-ui-react';
+import { Image, Responsive, Reveal, Grid } from 'semantic-ui-react';
 
 export default class AboutSection extends Component {
+
     render() {
         return (
             <div>
-            <div className="ui container about">
-                    <div className="about-image">
-                            <Image src={require("../../Media/portrait.jpg")} alt="portrait" size="medium"/>
-                        <p>
-                            You're such a fuckin' ho
-                                When the first time they ask you if you want sparklin' or still?
-                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
-                                You're such a fuckin' h—
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                        </p>
-                    </div>
-                </div>
+                <Grid>
+                    <Grid.Row>
+                        <div className="ui container about">
+                            <div className="about-image">
+                                <Reveal animated='rotate left'>
+                                    <Reveal.Content visible>
+                                        <Image src={require("../../Media/portrait.jpg")} alt="portrait" size="small"/>
+                                    </Reveal.Content>
+                                    <Reveal.Content hidden>
+                                        <Image src={require("../../Media/portrait.jpg")} alt="portrait" size="small"/>
+                                    </Reveal.Content>
+                                </Reveal>
+                            </div>
+                            <div className="about-p">
+                                <p>
+                                    You're such a fuckin' ho
+                                        When the first time they ask you if you want sparklin' or still?
+                                        Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
+                                        You're such a fuckin' h—
+                                        I'm a sick fuck, I like a quick fuck (whoop!)
+                                        I'm a sick fuck, I like a quick fuck (whoop!)
+                                        I'm a sick fuck, I like a quick fuck (whoop!)
+                                        I'm a sick fuck, I like a quick fuck (whoop!)
+                                </p>
+                            </div>
+                        </div>
+                    </Grid.Row>
+                <Grid.Row>
                 <div className="ui container about">
                     <div className="about-image even">
-                        <Image src={require("../../Media/family.jpg")} alt="family" size="medium"/>
-                        <p>
-                            You're such a fuckin' ho
-                                When the first time they ask you if you want sparklin' or still?
-                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
-                                You're such a fuckin' h—
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                        </p>
+                        <Reveal animated='rotate right'>
+                            <Reveal.Content visible>
+                                <Image src={require("../../Media/family.jpg")} alt="family" size="massive"/>
+                            </Reveal.Content>
+                            <Reveal.Content hidden>
+                                <Image src={require("../../Media/family.jpg")} alt="family" size="massive"/>
+                            </Reveal.Content>
+                        </Reveal>
+                            <p>
+                                You're such a fuckin' ho
+                                    When the first time they ask you if you want sparklin' or still?
+                                    Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
+                                    You're such a fuckin' h—
+                                    I'm a sick fuck, I like a quick fuck (whoop!)
+                                    I'm a sick fuck, I like a quick fuck (whoop!)
+                                    I'm a sick fuck, I like a quick fuck (whoop!)
+                                    I'm a sick fuck, I like a quick fuck (whoop!)
+                            </p>
                     </div>
                 </div>
+                </Grid.Row>
+                <Grid.Row>
                 <div className="ui container about">
                     <div className="about-image">
-                        <Image src={require("../../Media/boys.jpg")} alt="friends" size="medium"/>
+                    <Reveal animated='rotate left'>
+                        <Reveal.Content visible>
+                        <Image src={require("../../Media/boys.jpg")} alt="friends" size="massive"/>
+                        </Reveal.Content>
+                        <Reveal.Content hidden>
+                        <Image src={require("../../Media/boys.jpg")} alt="friends" size="massive"/>
+                        </Reveal.Content>
+                    </Reveal>
                         <p>
                             You're such a fuckin' ho
                                 When the first time they ask you if you want sparklin' or still?
@@ -50,6 +80,8 @@ export default class AboutSection extends Component {
                         </p>
                     </div>
                 </div>
+                </Grid.Row>
+                    </Grid>
             </div>
         )
     }
