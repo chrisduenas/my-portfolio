c9d5f46ffcf26e111efdf17e22d1caf90cd6ea53
Christopher Duenas <chrisduenasonline@gmail.com>
2018-09-28 23:36:21 -0400

    Added pictures and css styles to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Image } from 'semantic-ui-react';
+import AboutSection from './AboutMe/AboutSection';
+import { Responsive } from 'semantic-ui-react';
 import '../CSS/AboutMe.css';
 
 
@@ -10,51 +11,11 @@ export default class AboutMe extends Component {
                 <div className="about-header">
                     <h2>About me</h2>
                 </div>
-                <div className="ui container about">
-                    <div className="about-image odd">
-                        <Image src={require("../Media/family.jpg")} alt="family" size="medium"/>
-                        <p>
-                            You're such a fuckin' ho
-                                When the first time they ask you if you want sparklin' or still?
-                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
-                                You're such a fuckin' h—
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                        </p>
-                    </div>
-                </div>
-                <div className="ui container about">
-                    <div className="about-image even">
-                        <Image src={require("../Media/family.jpg")} alt="family" size="medium"/>
-                        <p>
-                            You're such a fuckin' ho
-                                When the first time they ask you if you want sparklin' or still?
-                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
-                                You're such a fuckin' h—
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                        </p>
-                    </div>
-                </div>
-                <div className="ui container about">
-                    <div className="about-image odd">
-                        <Image src={require("../Media/family.jpg")} alt="family" size="medium"/>
-                        <p>
-                            You're such a fuckin' ho
-                                When the first time they ask you if you want sparklin' or still?
-                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
-                                You're such a fuckin' h—
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                                I'm a sick fuck, I like a quick fuck (whoop!)
-                        </p>
-                    </div>
-                </div>
+                <Responsive min-width={768}>
+                    <AboutSection/>
+                </Responsive>
+                <Responsive max-width={768}>
+                </Responsive>
             </div>
         )
     }
---
+++ src/components/AboutMe/AboutSection.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Image } from 'semantic-ui-react';
+
+export default class AboutSection extends Component {
+    render() {
+        return (
+            <div>
+            <div className="ui container about">
+                    <div className="about-image">
+                        <Image src={require("../../Media/portrait.jpg")} alt="portrait" size="medium"/>
+                        <p>
+                            You're such a fuckin' ho
+                                When the first time they ask you if you want sparklin' or still?
+                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
+                                You're such a fuckin' h—
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                        </p>
+                    </div>
+                </div>
+                <div className="ui container about">
+                    <div className="about-image even">
+                        <Image src={require("../../Media/family.jpg")} alt="family" size="medium"/>
+                        <p>
+                            You're such a fuckin' ho
+                                When the first time they ask you if you want sparklin' or still?
+                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
+                                You're such a fuckin' h—
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                        </p>
+                    </div>
+                </div>
+                <div className="ui container about">
+                    <div className="about-image">
+                        <Image src={require("../../Media/boys.jpg")} alt="friends" size="medium"/>
+                        <p>
+                            You're such a fuckin' ho
+                                When the first time they ask you if you want sparklin' or still?
+                                Why you try to act like you was drinkin' sparklin' water 'fore you came out here?
+                                You're such a fuckin' h—
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                                I'm a sick fuck, I like a quick fuck (whoop!)
+                        </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
